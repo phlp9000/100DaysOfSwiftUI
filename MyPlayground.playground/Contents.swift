10af5ff0a5e282c0e1e92f3711c1ffc19e0d743d
phlp9000 <bbbbaer@gmail.com>
2024-02-05 20:17:21 +0100

    Initial

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,49 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Hans"
+name = "Willi"
+name = "Marc"
+
+let character = "Ted"
+
+var playerName = "Sam"
+print(playerName)
+
+playerName = "Michael"
+print(playerName)
+
+playerName = "Christian"
+print(playerName)
+
+print(character)
+
+let managerName = "Daniela"
+let catBreed = "British Shorthair"
+
+let actor = "Vin Diesel"
+let filename = "data.jpg"
+
+print(actor.count)
+print(actor.uppercased())
+
+print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 2
+let doubledScore = score * 2
+let halvedScore = score / 2
+
+var counter = 10
+counter = counter + 5
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
